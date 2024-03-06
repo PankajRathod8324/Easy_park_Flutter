3d5a9118c5a3c2864b742eb4821d8b5b3444ab1c
PankajRathod8324 <96436975+PankajRathod8324@users.noreply.github.com>
2024-03-06 22:01:53 +0530

    payment fix commit

--- lib/view/home/payment.dart
+++ lib/view/home/payment.dart
@@ -132,3 +132,34 @@ class _PaymentScreenState extends State<PaymentScreen> {
     });
   }
 }
+
+// Test Card Details
+// Debit Card
+// anchor image
+// “card_number”: “4242424242424242”,
+// “card_type”: “DEBIT_CARD”,
+// “card_issuer”: “VISA”,
+// “expiry_month”: 12,
+// “expiry_year”: 2023,
+// “cvv”: “936”
+
+// Credit Card
+// anchor image
+// “card_number”: “4208585190116667”,
+// “card_type”: “CREDIT_CARD”,
+// “card_issuer”: “VISA”,
+// “expiry_month”: 06,
+// “expiry_year”: 2027,
+// “cvv”: “508”
+
+// Note: The OTP to be used on the Bank Page: 123456
+
+// How to verify Net Banking Flow
+// anchor image
+// Always use “bankId”: “SBIN” for testing purposes in the request payload of PAY API.
+
+// Username: test
+// Password: test
+// and click “Submit” on the next screen.
+
+// Note: For testing transactions, the lower amount limit is defined as Rs.1/- and the upper amount limit is defined as Rs.1000/-. Please make sure your transaction requests are within these limits.
